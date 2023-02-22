64142b70321a22f3c82aa5be4b13fa9709a7315e
Choiyu330 <jenny4063@naver.com>
2023-02-22 18:01:26 +0900

    chore: spring 초기세팅

---
+++ src/test/java/com/spammayo/spam/SpamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spammayo.spam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

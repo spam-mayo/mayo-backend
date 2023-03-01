0632267bac798f07b31fe7be1af1652a1389ff65
kimmj13 <110887976+kimmj13@users.noreply.github.com>
2023-03-01 15:47:26 +0900

    feat: 스터디 CRUD (#10)
    * setting : cors 설정
    
    * setting : 공통 기능 초기 셋팅
    
    * setting : 예외 처리
    
    * setting : 공통 기능 초기 셋팅
    
    * setting : build.gradle 세팅
    
    * feat: 보안/인증 기본 설정 추가
    
    * feat: 회원 CURD, 로그인 기능
    
    * chore: 예외 코드 추가
    
    * chore: 회원 이름 유효성 로직 수정
    
    * feat: 로그아웃, 토큰 재발급, 이메일 인증 기능 추가
    
    * chore: 인증 api 변경
    
    * feat: 스택 기능 추가 및 회원과 연결
    
    * chore: 설정 변경
    
    * chore: 설정 변경
    
    * feat: 스터디 CRUD 추가

--- src/main/java/com/spammayo/spam/stack/mapper/StackMapper.java
+++ src/main/java/com/spammayo/spam/stack/mapper/StackMapper.java
@@ -9,5 +9,5 @@ import java.util.List;
 @Mapper(componentModel = "spring")
 public interface StackMapper {
 
-    List<StackDto.ResponseDto> stacksToResponseDto(List<Stack> stacks);
+    List<StackDto> stacksToResponseDto(List<Stack> stacks);
 }

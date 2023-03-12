8c6cfa5a424642f652c751f4f53cc6c6cddcbc6f
Choiyu330 <jenny4063@naver.com>
2023-03-13 02:44:14 +0900

    refactor: 연관관계 메소드 추가

--- src/main/java/com/spammayo/spam/task/entity/Task.java
+++ src/main/java/com/spammayo/spam/task/entity/Task.java
@@ -1,11 +1,14 @@
 package com.spammayo.spam.task.entity;
 
+import com.spammayo.spam.studycomment.entity.StudyComment;
 import com.spammayo.spam.study.entity.Study;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @Getter
 @Setter
@@ -23,8 +26,16 @@ public class Task {
     @Column(nullable = false)
     private String taskDate;
 
+    @OneToMany(mappedBy = "task", cascade = CascadeType.REMOVE)
+    private List<StudyComment> studyComments = new ArrayList<>();
+
     @ManyToOne
     @JoinColumn(name = "STUDY_ID")
     private Study study;
+
+    public void addStudyComment(StudyComment studyComment) {
+        studyComments.add(studyComment);
+        studyComment.setTask(this);
+    }
 
 }
